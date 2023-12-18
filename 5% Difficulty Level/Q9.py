aae1e3293590b0c26d25545dc49bcaedbc96c66d
ray074 <147860006+ray074@users.noreply.github.com>
2023-12-18 13:13:49 +0000

    Add files via upload

---
+++ 5% Difficulty Level/Q9.py
@@ -0,0 +1,20 @@
+def findSpecialPyTriplet():
+    
+    valid = set()
+
+    for a in range(3, 2000):
+        for b in range(3, 2000):
+            c = sqrt(a**2 + b**2)
+            if c.is_integer() is True and a < b < c:
+                valid.add((a, b, int(c)))
+
+    for triple in valid:
+        if sum(triple) == 1000:
+            total = 1
+            for num in triple:
+                total *= num
+            return total
+    
+print(specialPyTriplet())
+
+# Answer: 31875000
