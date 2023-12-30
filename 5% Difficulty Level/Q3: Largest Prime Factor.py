cb2e9172ba8c9bfa744f4a8da4b890e94755646d
Reyansh Tellis <147860006+ray074@users.noreply.github.com>
2023-12-30 12:01:56 +0000

    Update Q3: Largest Prime Factor.py

--- 5% Difficulty Level/Q3: Largest Prime Factor.py
+++ 5% Difficulty Level/Q3: Largest Prime Factor.py
@@ -3,7 +3,8 @@ def isPrime(num):
         if num % divisor == 0:
             return False
     return True
-    
+
+
 num = 600851475143
 print(max([x for x in range(2, int(num ** 0.5) + 1) if num % x == 0 and isPrime(x)]))
 
